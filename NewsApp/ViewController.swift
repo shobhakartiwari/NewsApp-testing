33627c678c370d6e4c51481ca99e0765b6488078
Mouli Agastya <saimouli.agastya@gmail.com>
2026-09-14 11:37:52 -0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mouli Agastya on 9/14/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
