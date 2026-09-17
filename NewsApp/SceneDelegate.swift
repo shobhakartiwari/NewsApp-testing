f7a82df1b4ba3bfcfabde48a0a3f90c203056823
Shobhakar Tiwari <st.shubh.tiwari@gmail.com>
2026-09-17 10:11:43 -0500

    Walmart-13239: added documentation

--- NewsApp/SceneDelegate.swift
+++ NewsApp/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         var viewModel: NewsHomeViewModelProtocol
-        
+        /// Checking the internet connectivity
         if isInternetAvailable {
             viewModel = NewsHomeViewModel(objNetwork: NetworkManager.shared)
         }
